459115f5db440386cef052f9c5055376f6b578bb
chris-s-harper <chris.harper@loop.colum.edu>
2018-01-25 17:35:55 -0600

    In Class Work Session Part 4
    Did:
    - Added main menu that is capable of bringing up an instructions screen,
    credits screen, and quiting the application.
    - Quit script won't be destroyed on load, so player should be able to
    quit at any point in the game potentially
    
    ToDo:
    - Clean up Main Menu screen, make it look nicer and more involve
    - Have main menu go into random microgame, which probably involves using
    the script that changes the scenes between microgames.

---
+++ MicroGames/Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    public List<GameObject> MainMenuPanels = new List<GameObject>();
+
+	// Use this for initialization
+	void Start ()
+    {
+        DisableAllPanelsExceptForTitle();
+	}
+
+    private void DisableAllPanelsExceptForTitle()
+    {
+        DeactivateAllPanels();
+        MainMenuPanels[0].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+
+    public void ChangePanel(GameObject panelToChangeTo)
+    {
+        DeactivateAllPanels();
+        panelToChangeTo.SetActive(true);
+    }
+
+    private void DeactivateAllPanels()
+    {
+        foreach (GameObject panel in MainMenuPanels)
+        {
+            panel.SetActive(false);
+        }
+    }
+}

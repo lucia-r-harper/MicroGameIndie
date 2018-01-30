c396d3913bdcefd03b32ead48eda23c466f178ab
Lucia-PC\lrhar <lucia.harper@loop.colum.edu>
2018-01-30 02:41:46 -0600

    Finished for the night
    Did:
    - Reorginizing of scripts
    - Changed build settings for tomorrow's class
    - Made metagame reset on game over
    
    ToDo:
    - Fix glitches
    - Make more microgames

---
+++ MicroGames/Assets/Scripts/MicroGameSpecific/TargetShoot/ShotFired.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotFired : MonoBehaviour
+{
+    private int speed = 1;
+    private Timer timer;
+
+    // Use this for initialization
+    void Start ()
+    {
+        timer = GameObject.FindObjectOfType<Timer>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    private void FixedUpdate()
+    {
+        ShootUp();
+    }
+
+    private void ShootUp()
+    {
+        transform.Translate(0.0f, speed, 0);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "border" && timer.MicroGameState == PlayingState.Playing)
+        {
+            //currentTimer.ChangePlayingState(PlayingState.Lost);
+            timer.MicroGameState = PlayingState.Lost;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        timer.MicroGameState = PlayingState.Won;
+    }
+}

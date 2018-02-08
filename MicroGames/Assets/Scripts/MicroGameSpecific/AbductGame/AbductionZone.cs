4b62a167cd0e7c7b03b705138f28103ef6427046
chris-s-harper <chris.harper@loop.colum.edu>
2018-02-08 15:20:08 -0600

    Pre-Class Work Day
    Did:
    - Laid foundation for "abductgame"
    
    ToDo:
    - Finish "abductgame"
    - Scale Difficulty

---
+++ MicroGames/Assets/Scripts/MicroGameSpecific/AbductGame/AbductionZone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbductionZone : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "abductee")
+        {
+            Abudct(collision.gameObject);
+        }
+    }
+
+    private void Abudct(GameObject gameObject)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ MicroGames/Assets/Scripts/MicroGameSpecific/AbductGame/PressSpaceToAbduct.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressSpaceToAbduct : MonoBehaviour
+{
+    public string ActivateAbductModeButton;
+    //public GameObject AbductionZone;
+    private AbductionZone abductionZone;
+    private PlayerMove playerMove;
+    private float defaultMoveSpeed;
+	// Use this for initialization
+	void Start ()
+    {
+        playerMove = GetComponent<PlayerMove>();
+        defaultMoveSpeed = playerMove.speed;
+        abductionZone = GetComponentInChildren<AbductionZone>();
+        abductionZone.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButton(ActivateAbductModeButton))
+        {
+            ActivateAbductionMode();
+        }
+        else
+        {
+            DeactivateAbductionMode();
+        }
+	}
+    private void ActivateAbductionMode()
+    {
+        abductionZone.gameObject.SetActive(true);
+        playerMove.speed = 0;
+    }
+
+    private void DeactivateAbductionMode()
+    {
+        abductionZone.gameObject.SetActive(false);
+        playerMove.speed = defaultMoveSpeed;
+    }
+
+}

b4e73486965e4d9752d10a463402eb8e546c8abd
Lucia-PC\lrhar <lucia.harper@loop.colum.edu>
2018-02-28 16:49:19 -0600

    4:47
    Did:
    - Added new UI buttons
    - Started work on button animation script
    
    ToDo:
    - Finish sidestep
    - Add animations to planetary defense
    - Finish button animations

---
+++ MicroGames/Assets/Scripts/Generics/ButtonAnimator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonAnimator : MonoBehaviour
+{
+    private Button button;
+    private Animator animator;
+    private BaseEventData baseEvent;
+
+    // Use this for initialization
+    void Start ()
+    {
+        button = GetComponent<Button>();
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //UpdateAnimations();
+	}
+
+    //private void UpdateAnimations()
+    //{
+    //    if (button. == true)
+    //    {
+
+    //    }
+    //}
+}

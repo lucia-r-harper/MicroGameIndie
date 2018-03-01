845f95b825812801e86ca83dc647007f39b4ae38
Lucia-PC\lrhar <lucia.harper@loop.colum.edu>
2018-02-28 20:15:02 -0600

    Before Dinner
    Did:
    - Added Animated buttons
    
    ToDo:
    - Finish Sidestep
    - Add difficulty to games w/o difficulty

---
+++ MicroGames/Assets/Scripts/AnimatedButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Animator))]
+public class AnimatedButton : Button
+{
+    private new Animator animator;
+    private BaseEventData baseEvent;
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (this.IsHighlighted(baseEvent) == true)
+        {
+            ChangeAnimationState(1);
+        }
+
+        if (this.IsPressed() == true)
+        {
+            ChangeAnimationState(2);
+        }
+	}
+
+    public override void OnDeselect(BaseEventData eventData)
+    {
+        base.OnDeselect(eventData);
+        ChangeAnimationState(0);
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        base.OnPointerDown(eventData);
+        ChangeAnimationState(2);
+    }
+
+    public void ChangeAnimationState(int intToChangeTo)
+    {
+        animator.SetInteger("buttonstate", intToChangeTo);
+    }
+}

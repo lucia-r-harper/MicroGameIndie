fc54e07a4b8ebb4a903c23ef65f35fc86fb43e0c
Lucia-PC\lrhar <lucia.harper@loop.colum.edu>
2018-03-01 00:59:13 -0600

    1 AM Work Sesh
    Did:
    - PlanetDefense does in fact scale
    - Meteors explode and fade out in planetdefense
    - Planet explodes and launches chunks in planetdefense
    
    ToDo:
    - Fix bugs
    - Polish

--- MicroGames/Assets/Scripts/MicroGameSpecific/PlanetDefense/Meteor.cs
+++ MicroGames/Assets/Scripts/MicroGameSpecific/PlanetDefense/Meteor.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class Meteor : MonoBehaviour
 {
     private Transform target;
+    private SpriteRenderer spriteRenderer;
     private float speed = 0.75f;
+    private bool isDestroyed = false;
     private const float difficultyScaleRate = 20;
+
+    private const float explosionFadeOutTime = 0.2f;
+    private float explosionAlphaValue = 255;
+    public Sprite ExplosionSprite;
     public Transform Target
     {
         set
@@ -17,6 +23,7 @@ public class Meteor : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         speed += MetaGameManager.Difficulty / difficultyScaleRate;
 	}
 	
@@ -24,10 +31,18 @@ public class Meteor : MonoBehaviour
 	void Update ()
     {
         transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);
-	}
+        if (isDestroyed)
+        {
+            float newAlpha = Mathf.SmoothDamp(spriteRenderer.color.a, 0, ref explosionAlphaValue, explosionFadeOutTime);
+            spriteRenderer.color = new Color(255, 255, 255, newAlpha);
+        }
+    }
 
     public void BlowUp()
     {
-        gameObject.SetActive(false);
+        //gameObject.SetActive(false);
+        spriteRenderer.sprite = ExplosionSprite;
+        isDestroyed = true;
+        speed = 0;
     }
 }
--- MicroGames/Assets/Scripts/MicroGameSpecific/PlanetDefense/Planet.cs
+++ MicroGames/Assets/Scripts/MicroGameSpecific/PlanetDefense/Planet.cs
@@ -1,27 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
+    public Rigidbody2D[] planetChunks;
+
     private Timer timer;
-	// Use this for initialization
-	void Start ()
+    private SpriteRenderer spriteRenderer;
+
+    private const float explosionFadeOutTime = 0.2f;
+    private float explosionAlphaValue = 255;
+    public Sprite ExplosionSprite;
+    private bool isDestroyed = false;
+
+    private const float driftForceMin = 25;
+    private const float driftForceMax = 75;
+    // Use this for initialization
+    void Start ()
     {
         timer = FindObjectOfType<Timer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        if (isDestroyed == true)
+        {
+            FadeOutExplosion();
+        }
 	}
+
+    private void FadeOutExplosion()
+    {
+        float newAlpha = Mathf.SmoothDamp(spriteRenderer.color.a, 0, ref explosionAlphaValue, explosionFadeOutTime);
+        spriteRenderer.color = new Color(255, 255, 255, newAlpha);
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<Meteor>() != null && timer.MicroGameState == PlayingState.Playing)
         {
+            Explode();
             timer.MicroGameState = PlayingState.Lost;
+        }
+    }
+
+    private void Explode()
+    {
+        spriteRenderer.sprite = ExplosionSprite;
+        isDestroyed = true;
+        foreach (Rigidbody2D chunk in planetChunks)
+        {
+            Vector2 forceToAdd = new Vector2(UnityEngine.Random.Range(driftForceMin, driftForceMax), UnityEngine.Random.Range(driftForceMin, driftForceMax));
+            chunk.AddForce(forceToAdd);
         }
     }
 }

d7460f0aa0548022f6feaace70f91c571a6a5864
Lucia-PC\lrhar <lucia.harper@loop.colum.edu>
2018-03-05 23:44:50 -0600

    Monday Night Work Sesh
    Did:
    - Made Pause Button
    
    ToDo:
    - Refactor and clean

---
+++ MicroGames/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour
+{
+    public bool IsPaused = false;
+    public GameObject popUp;
+    public GameObject ContinueButton;
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+
+    private void OnEnable()
+    {
+        //popUp = GetComponentInChildren<GameObject>();
+        popUp.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (IsPaused)
+            {
+                UnPause();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    public void UnPause()
+    {
+        IsPaused = false;
+        popUp.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    private void Pause()
+    {
+        IsPaused = true;
+        Debug.Log(IsPaused);
+        popUp.SetActive(true);
+        Time.timeScale = 0;
+        FindObjectOfType<EventSystem>().SetSelectedGameObject(ContinueButton);
+    }
+}

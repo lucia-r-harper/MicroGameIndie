d1bd294e841e3e583d4e49b03e0da3971a24aa8e
chris-s-harper <chris.harper@loop.colum.edu>
2018-03-13 17:04:18 -0500

    5:02 PM
    Did:
    - Added kyle's files for ponguin
    - Created an anouncement system that will anounce certain phrases when
    the game starts, is lost, or won
    - Added audio mixer that distinguishes between VO, SFX, and Music
    
    ToDo:
    - Impliment Kyle's Art
    - Add all the audio clips

---
+++ MicroGames/Assets/Scripts/Generics/VOAnnouncementScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VOAnnouncementScript : MonoBehaviour
+{
+    public AudioClip[] audioClips = new AudioClip[3];
+    private AudioSource audioSource;
+
+    private Timer timer;
+    private bool hasAnnouncementPlayed = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+        timer = FindObjectOfType<Timer>();
+        playAnnouncement(audioClips[0]);
+        hasAnnouncementPlayed = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        switch (timer.MicroGameState)
+        {
+            case PlayingState.Playing:
+                break;
+            case PlayingState.Lost:
+                if (audioSource.isPlaying == false && hasAnnouncementPlayed == false)
+                {
+                    playAnnouncement(audioClips[1]);
+                }
+                break;
+            case PlayingState.Won:
+                if (audioSource.isPlaying == false && hasAnnouncementPlayed == false)
+                {
+                    playAnnouncement(audioClips[2]);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void playAnnouncement(AudioClip announcementToPlay)
+    {
+        audioSource.clip = announcementToPlay;
+        audioSource.Play();
+        hasAnnouncementPlayed = true;
+    }
+}

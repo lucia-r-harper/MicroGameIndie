84bdf7b03c30153109dd507c81762bde51cf0c0f
chris-s-harper <chris.harper@loop.colum.edu>
2018-04-10 16:06:53 -0500

    4:05 PM
    Did:
    - Added win animation for planetary defense
    
    ToDo:
    - Add lose animation for planteray defense as well as other games

---
+++ MicroGames/Assets/Scripts/Generics/ObjectFollower.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFollower : MonoBehaviour
+{
+
+    public GameObject target;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        FollowTarget();
+	}
+
+    private void FollowTarget()
+    {
+        transform.position = target.transform.position;
+    }
+}

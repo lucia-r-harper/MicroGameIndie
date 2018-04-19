63a67fca80c7d818dc8abd58b0555207eabccd57
Lucia-PC\lrhar <lucia.harper@loop.colum.edu>
2018-04-18 21:57:57 -0500

    9:52
    Added highscore system and new songs from patrick

--- MicroGames/Assets/Scripts/Managers/HighScoreManager.cs
+++ MicroGames/Assets/Scripts/Managers/HighScoreManager.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
+using UnityEngine.UI;
 
 public class HighScoreManager : MonoBehaviour
 {
-    //figure me out with XML so people can't just hack into it, unless the resource folder is restricted then just use a .txt file because that's all you need
+    public Text HighScoreText;
+    public Animator HighScoreNotificationAnimator;
+
     private int highScore = 0;
     private int sessionScore;
 
@@ -18,36 +21,80 @@ public class HighScoreManager : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
-        //should work in build, might not work in editor
-        path = Application.dataPath + "/Resources/_saveData";
+        CheckForDirectoryAndFileExistance();
+
+        SetDataPath();
+
         GetCurrentSessionScore();
         GetHighScore();
-        SetHighScore();
-	}
+        CheckForNewHighScore();
+    }
+
+    private void CheckForDirectoryAndFileExistance()
+    {
+        //Checks to see if directory doesn't exists, creates it.
+        if (!Directory.Exists(Application.dataPath + "/_saveData"))
+        {
+            Directory.CreateDirectory(Application.dataPath + "/_saveData");
+            SetDataPath();
+            SetNewHighScore();
+        }
+        else
+        {
+            SetDataPath();
+        }
+        //Checks to see if file doesn't exist in the directory
+        if (!File.Exists(Path.Combine(path, "saveData.xml")))
+        {
+            SetDataPath();
+            SetNewHighScore();
+        }
+    }
 
     private void GetHighScore()
     {
-        //read from a file, written out by this scripts
+        XmlSerializer serializer = new XmlSerializer(typeof(int));
+        FileStream stream = new FileStream(Path.Combine(path, "saveData.xml"), FileMode.Open);
+        highScore = (int)serializer.Deserialize(stream);
+        stream.Close();
+        UpdateHighScoreText();
     }
 
-    private void SetHighScore()
+
+    private void CheckForNewHighScore()
     {
         if (sessionScore > highScore)
         {
             highScore = sessionScore;
+            HighScoreNotificationAnimator.SetBool("newhighscore", true);
             Debug.Log("New high score!!!");
+            SetNewHighScore();
 
-            XmlSerializer serializer = new XmlSerializer(typeof(int));
-            FileStream stream = new FileStream(path, FileMode.Create);
-            serializer.Serialize(stream, highScore);
-            stream.Close();
+            UpdateHighScoreText();
         }
+    }
+
+    private void SetNewHighScore()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(int));
+        FileStream stream = new FileStream(Path.Combine(path, "saveData.xml"), FileMode.Create);
+        serializer.Serialize(stream, highScore);
+        stream.Close();
+    }
+
+    private void SetDataPath()
+    {
+        path = Application.dataPath + "/_saveData";
+    }
+
+    private void UpdateHighScoreText()
+    {
+        HighScoreText.text = "High Score: " + highScore.ToString();
     }
 
     private void GetCurrentSessionScore()
     {
         sessionScore = MetaGameManager.Score;
-        Debug.Log(sessionScore.ToString());
     }
 
     // Update is called once per frame
